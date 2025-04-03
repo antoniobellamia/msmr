80d685c93e076d42040530aff64253a4d9254fc4
antoniobellamia <bellamiaantonio@gmail.com>
2025-04-03 23:30:16 +0200

    File database.php aggiunto

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+
+    $geoHost = "127.0.0.1";  # Indirizzo "localhost"
+    $geoUser = "root";       # L'utente del DBMS
+    $geoPass = "";           # La password dell'utente del DBMS
+    $geoName = "geografia"; # Il nome del database da selezionare
+    
+    $msHost = "127.0.0.1";  # Indirizzo "localhost"
+    $msUser = "root";       # L'utente del DBMS
+    $msPass = "";           # La password dell'utente del DBMS
+    $msName = "indirizzi"; # Il nome del database da selezionare
+    
+    
+    // Effettuo la connessione al DB
+    $geoConn = mysqli_connect(
+        $geoHost,
+        $geoUser,
+        $geoPass,
+        $geoName
+    );
+    
+    $msConn = mysqli_connect(
+        $msHost,
+        $msUser,
+        $msPass,
+        $msName
+    );
