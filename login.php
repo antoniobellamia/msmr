41a2942d2ca3dae14038ba3e3733db3246eb677e
antoniobellamia <bellamiaantonio@gmail.com>
2025-04-04 18:18:31 +0200

    Link pulsante login

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <?php include_once $_SERVER['DOCUMENT_ROOT']. '/msmr/components/navbar-out.php' ?>
+    <!--BODY-->
+
+                   <div class="w3-panel w3-red w3-display-container " style="width:50%;">
+                 <span onclick="this.parentElement.style.display='none'" class="w3-button w3-large w3-display-topright">×</span>
+                 <h3>Danger!</h3>
+                 <p>Red often indicates a dangerous or negative situation.</p>
+
+</div>
+
+                <div class="w3-card-4" style="width:50%;">
+
+
+
+
+                   <header class="w3-container">
+                     <h1>Header</h1>
+                   </header>
+
+                   <div class="w3-container">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                   </div>
+
+                 </div>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT']. '/msmr/components/footer.php' ?>
+
+
+
