3116ee1c94b26235f1c7b2901bff87f2414c4862
antoniobellamia <bellamiaantonio@gmail.com>
2025-04-09 17:43:04 +0200

    Update success.php

--- auth/success.php
+++ auth/success.php
@@ -1,1 +1,17 @@
-OK
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HOMEPAGE</title>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/msmr/components/navbar-out.php' ?>
+    <!--BODY-->
+
+    <div class="align-center">
+
+        <h1 style="font-size: 5rem"><i class="fa-regular fa-thumbs-upX"></i> <i class="fa-solid fa-checkX"></i><h1>
+
+        <h1 style="font-size: 3rem">200 OK.</h1>
+
+    </div>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/msmr/components/footer.php' ?>
