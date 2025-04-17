d43972ea571d1235cd186e62bfb4e75d794962b1
antoniobellamia <bellamiaantonio@gmail.com>
2025-04-17 16:54:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php include_once $_SERVER['DOCUMENT_ROOT']. '/msmr/components/session-con.php' ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT']. '/msmr/components/session-con.php' ;
+
+if(isset($_SESSION['tipo']))
+    switch($_SESSION['tipo']){
+        case 0: header("Location: //".$_SERVER['SERVER_NAME']."/msmr/cliente/");  break;
+        case 1: header("Location: //".$_SERVER['SERVER_NAME']."/msmr/admin/");  break;
+        case 2: header("Location: //".$_SERVER['SERVER_NAME']."/msmr/corriere/");  break;
+        case 3: header("Location: //".$_SERVER['SERVER_NAME']."/msmr/admin-corrieri/");  break;
+    }
+
+
+
+
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -12,6 +26,7 @@
     <form >
 
     <input type="search" placeholder="Cerca un ID spedizione..."></input>
+    <p> - - id_ordine, progressivo, stato DA STATO</p>
 
 </form>
 
