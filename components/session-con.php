7a9424b7f0a308718d13590ffe9522bd23eeed6d
antoniobellamia <bellamiaantonio@gmail.com>
2025-04-17 14:46:42 +0200

    fix sessioni first access

--- components/session-con.php
+++ components/session-con.php
@@ -2,6 +2,7 @@
     if (empty($_SESSION["id"])) {
         session_destroy();
     }
+    if (isset($_SESSION["firstAccess"]) && $_SESSION["firstAccess"] = true) header("Location: //".$_SERVER['SERVER_NAME'] . "/msmr/auth/logout.php");
 
     // <?php include_once $_SERVER['DOCUMENT_ROOT']. '/msmr/components/session-con.php' >
 ?>
